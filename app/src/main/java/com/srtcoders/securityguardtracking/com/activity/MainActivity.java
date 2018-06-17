7c2b91da5459c5cb9d7b5d100dcf96fa603b4845
shahupreeti <smartcoders25@gmail.com>
2018-06-17 12:50:59 +0530

    create view pager.

--- app/src/main/java/com/srtcoders/securityguardtracking/com/activity/MainActivity.java
+++ app/src/main/java/com/srtcoders/securityguardtracking/com/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.srtcoders.securityguardtracking;
+package com.srtcoders.securityguardtracking.com.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.srtcoders.securityguardtracking.R;
 
 public class MainActivity extends AppCompatActivity {
 
