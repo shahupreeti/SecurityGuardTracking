5f0a475b3636fc690df8e40e536556cf24d672e2
shahupreeti <smartcoders25@gmail.com>
2018-06-18 21:25:18 +0530

    Changes on Login and Welcome Activity.

---
+++ app/src/main/java/com/srtcoders/securityguardtracking/com/activity/LoginActivity.java
@@ -0,0 +1,25 @@
+package com.srtcoders.securityguardtracking.com.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+import com.srtcoders.securityguardtracking.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText user_id_EditText,password_EditText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        initialization();
+    }
+
+    public void initialization(){
+        user_id_EditText = (EditText) findViewById(R.id.user_id_et);
+        user_id_EditText.setHint("#fff");
+        password_EditText = (EditText) findViewById(R.id.password_et);
+        password_EditText.setHint("#fff");
+    }
+}
